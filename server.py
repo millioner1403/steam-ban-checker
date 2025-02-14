815dbf70578f1e4adc33cc2a446e16cd64b8461d
millioner1403 <rgh5533@gmail.com>
2025-02-15 00:48:06 +0300

    Add

--- server.py
+++ server.py
@@ -58,4 +58,5 @@ def get_bans():
         return {"error": str(e)}, 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Railway сам задает порт
+    app.run(host='0.0.0.0', port=port)
